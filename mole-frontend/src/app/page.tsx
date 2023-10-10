f5f36235155fd0b094f3715ec2737c9f10765a4a
Marco Magnanini <marcomag416@gmail.com>
2023-10-10 12:20:30 +0200

    reformat

--- mole-frontend/src/app/page.tsx
+++ mole-frontend/src/app/page.tsx
@@ -18,26 +18,26 @@ import majoImg from '@/components/static_media/majo.png';
 
 
 const matches = [
-  {id:"1", title:"Maj - Daz", description:"Questo è un esempio di partita. Bal bla bla bla", datetime:"20/10 21:30", img:matchImg, initial:false},
-  {id:"2", title:"Alf - Cat", description:"Questo è un esempio di partita. Bal bla bla bla", datetime:"20/10 20:00", img:matchImg, initial:false},
-  {id:"3", title:"Gio - Maj", description:"Questo è un esempio di partita. Bal bla bla bla", datetime:"27/10 21:30", img:matchImg, initial:false},
-  {id:"4", title:"Cat - Gal", description:"Questo è un esempio di partita. Prima partita", datetime:"27/10 20:00", img:matchImg, initial:true},
-  {id:"5", title:"Gio - Daz", description:"Questo è un esempio di partita. Bal bla bla bla", datetime:"2/11 21:30", img:matchImg, initial:false},
-  {id:"6", title:"Gal - Alf", description:"Questo è un esempio di partita. Bal bla bla bla", datetime:"2/11 20:00", img:matchImg, initial:false},
-  {id:"7", title:"Daz - Maj", description:"Questo è un esempio di partita. Bal bla bla bla", datetime:"9/11 21:30", img:matchImg, initial:false},
+  { id: "1", title: "Maj - Daz", description: "Questo è un esempio di partita. Bal bla bla bla", datetime: "20/10 21:30", img: matchImg, initial: false },
+  { id: "2", title: "Alf - Cat", description: "Questo è un esempio di partita. Bal bla bla bla", datetime: "20/10 20:00", img: matchImg, initial: false },
+  { id: "3", title: "Gio - Maj", description: "Questo è un esempio di partita. Bal bla bla bla", datetime: "27/10 21:30", img: matchImg, initial: false },
+  { id: "4", title: "Cat - Gal", description: "Questo è un esempio di partita. Prima partita", datetime: "27/10 20:00", img: matchImg, initial: true },
+  { id: "5", title: "Gio - Daz", description: "Questo è un esempio di partita. Bal bla bla bla", datetime: "2/11 21:30", img: matchImg, initial: false },
+  { id: "6", title: "Gal - Alf", description: "Questo è un esempio di partita. Bal bla bla bla", datetime: "2/11 20:00", img: matchImg, initial: false },
+  { id: "7", title: "Daz - Maj", description: "Questo è un esempio di partita. Bal bla bla bla", datetime: "9/11 21:30", img: matchImg, initial: false },
 
 ];
 
 const teams = [
-  {id:"alf", name:"Alfieri", img:alfieriImg},
-  {id:"gob", name:"Gobetti", img:gobettiImg},
-  {id:"cat", name:"Cattaneo", img:cattaneoImg},
-  {id:"gob22", name:"Gobetti", img:gobetti22Img},
-  {id:"avo", name:"Avogadro", img:avogadroImg},
-  {id:"cav", name:"Cavour", img:cavourImg},
-  {id:"conv", name:"Covitto", img:convittoImg},
-  {id:"daze", name:"Dazeglio", img:dazeImg},
-  {id:"majo", name:"Majorana", img:majoImg},
+  { id: "alf", name: "Alfieri", img: alfieriImg },
+  { id: "gob", name: "Gobetti", img: gobettiImg },
+  { id: "cat", name: "Cattaneo", img: cattaneoImg },
+  { id: "gob22", name: "Gobetti", img: gobetti22Img },
+  { id: "avo", name: "Avogadro", img: avogadroImg },
+  { id: "cav", name: "Cavour", img: cavourImg },
+  { id: "conv", name: "Covitto", img: convittoImg },
+  { id: "daze", name: "Dazeglio", img: dazeImg },
+  { id: "majo", name: "Majorana", img: majoImg },
 ];
 const firstTeam = Math.round(teams.length / 2) - 2;
 
@@ -70,21 +70,21 @@ export default function Home() {
       <Typography variant='h2' align='center' gutterBottom>Le partite</Typography>
       <CardSlider>
         {matches.map((match, i) =>
-          <MatchCard 
-            key = {match.id}
-            img = {match.img}
-            url = {'/match/' + match.id}
-            initial = {match.initial}
-            title = {match.title}
-            description = {match.description}
-            datetime = {match.datetime}
+          <MatchCard
+            key={match.id}
+            img={match.img}
+            url={'/match/' + match.id}
+            initial={match.initial}
+            title={match.title}
+            description={match.description}
+            datetime={match.datetime}
           />
         )}
       </CardSlider>
-      <Container sx={{marginTop:"10px", marginBottom:"10px"}}>
-        <StandingTable title="Girone A" small/>
-        <StandingTable title="Girone B"/>
-        <StandingTable title="Girone C"/>
+      <Container sx={{ marginTop: "10px", marginBottom: "10px" }}>
+        <StandingTable title="Girone A" small />
+        <StandingTable title="Girone B" />
+        <StandingTable title="Girone C" />
       </Container>
     </>
   )
