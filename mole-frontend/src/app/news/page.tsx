c1086476aabe02f20b11e5be190ffc3807dcf0eb
Marco Magnanini <marcomag416@gmail.com>
2024-02-22 20:17:13 +0100

    news page

--- mole-frontend/src/app/news/page.tsx
+++ mole-frontend/src/app/news/page.tsx
@@ -1,7 +1,23 @@
-export default function News(){
+import { RelatedArticlesGrid, getAllArticles } from "@/components/relatedArticles";
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+import HeroHeader from "@/components/heroHeader";
+
+
+export default async function News(){
+    const news = await getAllArticles();
+    console.log(news);
     return(
-        <div>
-            <h1>News page</h1>
-        </div>
+        <>
+        <HeroHeader src="/DSC_0666-3.jpg">
+            <Typography variant="h1" color="white" textTransform="uppercase">Notizie</Typography>
+        </HeroHeader>
+        
+        <Container>
+            <RelatedArticlesGrid articles={news} xs={12} sm={6} md={4} lg={3}/>
+        </Container>
+        </>
     )
 }
--- mole-frontend/src/components/relatedArticles.tsx
+++ mole-frontend/src/components/relatedArticles.tsx
@@ -30,6 +30,12 @@ export interface relatedArticleInterface {
     }
 }
 
+export async function getAllArticles(): Promise<relatedArticleInterface[]>{
+    const path = `/api/articles?sort[0]=date:desc&populate[cover]=1&populate[article_tags][fields][0]=id&populate[article_tags][fields][1]=name&fields[0]=title&fields[1]=author&fields[2]=date&fields[3]=abstract&fields[4]=slug`;
+    const res  = await publicFetch(path);
+    return res.data;
+}
+
 export async function getRelatedArticles(tags : {id : number}[]) : Promise<relatedArticleInterface[]>{
     if(tags.length === 0){
         return [];
@@ -63,12 +69,12 @@ export default function RelatedArticles({articles, ...otherProps} : {articles: r
     );
 }
 
-export function RelatedArticlesGrid({articles, ...otherProps} : {articles: relatedArticleInterface[], [index: string]: any}){
+export function RelatedArticlesGrid({articles, xs=12, sm=6, md=6, lg=6, ...otherProps} : {articles: relatedArticleInterface[], [index: string]: any}){
     return(
         <div>
             <Grid container spacing={1} sx={{padding:"5px"}}>
             {articles.map((article : relatedArticleInterface, i : number) => 
-                <Grid md={6}>
+                <Grid lg={lg} md={md} sm={sm} xs={xs}>
                     <NewsCard
                         key={i}
                         title = {article.attributes.title}
