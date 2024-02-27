7bc3a09c4b591ff603ce12813841c82d7acb838f
Marco Magnanini <marcomag416@gmail.com>
2024-02-27 21:47:34 +0100

    update map component with useRef hook

--- mole-frontend/src/components/map.js
+++ mole-frontend/src/components/map.js
@@ -6,38 +6,39 @@ docs: https://developers.google.com/maps/documentation/javascript/reference/adva
 
 import Box from '@mui/material/Box';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import loader from '@/lib/googleMapsLoader';
 import generateGoogleMapsLink from '@/lib/generateGoggleMapsLink';
 
 const Map = ({ address, mapHeight="400px", id="map", ...props }) => {
-    const [map, setMap] = useState(null);
-    useEffect(() => {
-      loader.importLibrary('places', 'marker').then(() => {
-        const geocoder = new window.google.maps.Geocoder();
-        geocoder.geocode({ address }, (results, status) => {
-          if (status === 'OK') {
-            const mapOptions = {
-              center: results[0].geometry.location,
-              zoom: 15,
-              mapId: id,
-            };
-            const newMap = new window.google.maps.Map(
-              document.getElementById(id),
-              mapOptions
-            );
-            const marker = new window.google.maps.marker.AdvancedMarkerElement({
-              position: results[0].geometry.location,
-              map: newMap,
-            });
-            google.maps.event.addListener(marker, 'click', function() {
-              window.location.href = generateGoogleMapsLink(address);
-            });
-            setMap(newMap);
-          }
-        });
+  const googlemap = useRef(null);
+  const [map, setMap] = useState(null);
+  useEffect(() => {
+    loader.importLibrary('places', 'marker').then(() => {
+      const geocoder = new window.google.maps.Geocoder();
+      geocoder.geocode({ address }, (results, status) => {
+        if (status === 'OK') {
+          const mapOptions = {
+            center: results[0].geometry.location,
+            zoom: 15,
+            mapId: id,
+          };
+          const newMap = new window.google.maps.Map(
+            googlemap.current,
+            mapOptions
+          );
+          const marker = new window.google.maps.marker.AdvancedMarkerElement({
+            position: results[0].geometry.location,
+            map: newMap,
+          });
+          google.maps.event.addListener(marker, 'click', function() {
+            window.location.href = generateGoogleMapsLink(address);
+          });
+          setMap(newMap);
+        }
       });
-    }, [address]);
-    return <Box id={id} sx={{ height: mapHeight, ...props.sx }}></Box>;
-  };
+    });
+  }, [address]);
+  return <Box id={id} ref={googlemap} sx={{ height: mapHeight, ...props.sx }}></Box>;
+};
   export default Map;
