02a5428014903bd4c7890439b30e121394defaee
Marco Magnanini <marcomag416@gmail.com>
2024-03-06 23:17:38 +0100

    added promoter section in home page

--- mole-frontend/src/app/page.tsx
+++ mole-frontend/src/app/page.tsx
@@ -10,23 +10,83 @@ import Diversity1RoundedIcon from '@mui/icons-material/Diversity1Rounded';
 import ForestRoundedIcon from '@mui/icons-material/ForestRounded';
 import AccountBalanceRoundedIcon from '@mui/icons-material/AccountBalanceRounded';
 import WhereToVoteRoundedIcon from '@mui/icons-material/WhereToVoteRounded';
+import Paper from '@mui/material/Paper';
+import FormatQuoteRoundedIcon from '@mui/icons-material/FormatQuoteRounded';
+import ChangeHistoryRoundedIcon from '@mui/icons-material/ChangeHistoryRounded';
 
 
 export default function Home(){
     return(
         <>
-            <HeroHeader src="/DSC_0666-3.jpg">
-                <Typography variant="h1" color="white" textTransform="uppercase">{"Mole cup"}</Typography>
-                <Typography variant="h3" color="primary.main" textTransform="capitalize">{"Reale mutua"}</Typography>
+            <HeroHeader src="/DSC_0666-3.jpg" sx={{height: "66vh"}}>
+                <Stack alignItems="center" spacing={2}>
+                    <Typography variant="h1" color="white" textTransform="uppercase">{"Mole cup"}</Typography>
+                    <Typography variant="h3" color="primary.main" fontWeight={700} textTransform="capitalize">{"Reale mutua"}</Typography>
+                    <Typography variant="h6" color="secondary.contrastText" sx={{pt: "20px", textAlign:"center"}}>{"Dai banchi di scuola ai campi da calcio: il teatro del torneo più atteso dagli studenti"}</Typography>
+                </Stack>
             </HeroHeader>
-            <TwoSpanBlogSection src="/DSC_0666-3.jpg" sx={{height: {xs:"400px", sm:"300px", md:"300px"}}}>
+            <TwoSpanBlogSection src="/static/DSCF6614-Migliorato-NR.jpg" sx={{height: {xs:"400px", sm:"300px", md:"300px"}}}>
                 <Typography variant="h2" color="primary.main" fontWeight={500}>{"Chi siamo"}</Typography>
-                <Typography variant="body1">{"Mole Cup è un'associazione nata nel 2017 con l'obbiettivo di creare un torneo di calcio innovativo e coinvolgente tra le scuole superiori di Torino"}</Typography>
-                <Typography variant="body1">{"Dal 2025 la Molecup si espanderà in tutta Italia, con l'obbiettivo di creare un campionato nazionale che abbia un torneo in ogni grande città italiana, a partire da Torino, Firenze e Milano"}</Typography>
+                <Stack direction="row">
+                    <ChangeHistoryRoundedIcon sx={{transform: "rotate(90deg)", color: "primary.main"}}/>
+                    <Typography variant="body1">{"Mole Cup è un'associazione nata nel 2017 con l'obbiettivo di creare un torneo di calcio innovativo e coinvolgente tra le scuole superiori di Torino"}</Typography>
+                </Stack>
+                <Stack direction="row">
+                    <ChangeHistoryRoundedIcon sx={{transform: "rotate(90deg)", color: "primary.main"}}/>
+                    <Typography variant="body1">{"Dal 2025 la Molecup si espanderà in tutta Italia, con l'obbiettivo di creare un campionato nazionale che abbia un torneo in ogni grande città italiana, a partire da Torino, Firenze e Milano"}</Typography>
+                </Stack>
             </TwoSpanBlogSection>
             
             <FeaturesSection/>
+
+            <PromoterSection />
         </>
+    );
+}
+
+function PromoterSection(){
+    const promoters = [
+        {
+            img: "/static/giancarlo-antognoni.jpg",
+            name: "Giancarlo Antognoni",
+            description: "Dirigente sportivo, ex calciatore",
+            quote: "L'iniziativa ripercorre i veri valori dello sport in cui credo e in cui mi riconosco. \
+                Il torneo si svolge in maniera spontanea e genuina con l'impegno e la dedizione degli organizzatori.",
+        },
+        {
+            img: "/static/ciro-ferrara.jpg",
+            name: "Ciro Ferrara",
+            description: "Allenatore, ex calciatore",
+            quote: "Grazie ad un torneo simile io ho dato inizio alla mia carriera",
+        },
+    ]
+    return(
+        <Box component="section">
+                <Container sx={{textAlign: "center", mt:5, mb:10}}>
+                    <Grid container spacing={3}>
+                        <Grid xs={12}>
+                            <Typography variant="h2" color="primary.main" fontWeight={500}>{"Dicono di noi"}</Typography>
+                        </Grid>
+                        {promoters.map((promoter, idx) => 
+                            <Grid xs={12} md={6} key={idx}>
+                                <Box sx = {{padding: "10px"}}>
+                                    <Stack direction="column" spacing={2} alignItems="center">
+                                        <Avatar src={promoter.img} alt={promoter.name} sx={{ width: 70, height: 70 }}/>
+                                        <Typography variant="h4" color="text.primary" textTransform="uppercase">{promoter.name}</Typography>
+                                        <Typography variant="caption" textTransform="uppercase" color="primary.main">{promoter.description}</Typography>
+                                        <Stack direction="row" spacing={2}>
+                                            <FormatQuoteRoundedIcon sx={{color: "text.secondary", display:"inline-block"}}/>
+                                            <Typography variant="body1" color="text.secondary" sx={{display:"inline-block"}}>{promoter.quote}</Typography>
+                                            <FormatQuoteRoundedIcon sx={{color: "text.secondary", display:"inline-block"}}/>
+                                        </Stack>
+                                    </Stack>
+                                </Box>
+                            </Grid>
+                        )}
+                        
+                    </Grid>
+                </Container>
+            </Box>
     );
 }
 
