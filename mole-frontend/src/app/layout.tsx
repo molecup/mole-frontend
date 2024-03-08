5e63271a92d5549cb57e64ca4cbdef68dbbaf9f2
Marco Magnanini <marcomag416@gmail.com>
2024-03-08 14:11:22 +0100

    changed metadata decription

--- mole-frontend/src/app/layout.tsx
+++ mole-frontend/src/app/layout.tsx
@@ -10,7 +10,7 @@ import CookiesSnackbar from '@/components/cookiesSnackbar';
 //const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Mole cup - official site',
+  title: 'Mole cup - Sito Ufficiale',
   description: 'Sito ufficiale della Mole Cup',
 }
 
