42d4079c81ac83dd676b18a700bf867bfa01bb9d
Marco Magnanini <marcomag416@gmail.com>
2024-03-08 17:11:52 +0100

    fixed date bug

--- mole-frontend/src/lib/dateTimeText.ts
+++ mole-frontend/src/lib/dateTimeText.ts
@@ -1,6 +1,7 @@
 
 export default function dateTimeText(date : Date): [string, string]{
-    const timeTxt = `${("00" + date.getHours()).slice(-2)}:${("00" + date.getMinutes()).slice(-2)}`;
-    const dateTxt =  `${date.getDate()}/${date.getMonth()}`;
+    /*@ts-ignore */
+    const timeTxt = `${("00" + date.getHours("it-IT", {timeZone: 'Europe/Rome'})).slice(-2)}:${("00" + date.getMinutes()).slice(-2)}`;
+    const dateTxt =  `${date.getDate()}/${date.getMonth() + 1}`;
     return [dateTxt, timeTxt];
 }
