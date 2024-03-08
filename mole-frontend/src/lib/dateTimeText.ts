cca658e8c583f307da7aeb6cdb7898907237004e
Marco Magnanini <marcomag416@gmail.com>
2024-03-08 17:23:36 +0100

    fixed hour timezone rome

--- mole-frontend/src/lib/dateTimeText.ts
+++ mole-frontend/src/lib/dateTimeText.ts
@@ -1,7 +1,12 @@
 
 export default function dateTimeText(date : Date): [string, string]{
-    /*@ts-ignore */
-    const timeTxt = `${("00" + date.getHours("it-IT", {timeZone: 'Europe/Rome'})).slice(-2)}:${("00" + date.getMinutes()).slice(-2)}`;
+    let options = {
+        timeZone: 'Europe/Rome',
+      }
+    
+    const hour = date.toLocaleString('it-IT', {hour: '2-digit',   hour12: false, timeZone: 'Europe/Rome' })
+
+    const timeTxt = `${("00" + hour).slice(-2)}:${("00" + date.getMinutes()).slice(-2)}`;
     const dateTxt =  `${date.getDate()}/${date.getMonth() + 1}`;
     return [dateTxt, timeTxt];
 }
