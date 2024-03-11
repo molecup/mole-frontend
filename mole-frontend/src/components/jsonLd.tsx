3fead96f60a43829ca3431826f57a173f2d35698
Marco Magnanini <marcomag416@gmail.com>
2024-03-11 18:16:52 +0100

    fixed bug in jsonLd

--- mole-frontend/src/components/jsonLd.tsx
+++ mole-frontend/src/components/jsonLd.tsx
@@ -1,15 +1,12 @@
 import Script from 'next/script'
-import Head from 'next/head'
 import { Thing, WithContext } from 'schema-dts';
 
-export default function JsonLd<T extends Thing>(json: WithContext<T>){
+export default function JsonLd<T extends Thing>(json: WithContext<T>, id : string){
     return(
-        <Head>
-            <Script
-                type="application/ld+json"
-                dangerouslySetInnerHTML={{ __html: JSON.stringify(json) }}
-            />
-        </Head>
-        
+        <Script
+            type="application/ld+json"
+            id={id}
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(json) }}
+        />
     );
 }
