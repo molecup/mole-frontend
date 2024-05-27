731e9aa1e0edc5012f01e20f7276d2533552de11
Marco Magnanini <marcomag416@gmail.com>
2024-05-27 16:25:39 +0200

    feat: added external articles

--- mole-frontend/src/components/relatedArticles.tsx
+++ mole-frontend/src/components/relatedArticles.tsx
@@ -14,6 +14,8 @@ export interface relatedArticleInterface {
         author: string,
         abstract: string,
         slug: string,
+        externalArticle?: boolean,
+        externalArticleLink?: string,
         cover: {
             data? : {
                 id : number,
@@ -32,7 +34,7 @@ export interface relatedArticleInterface {
 }
 
 export async function getAllArticles(): Promise<relatedArticleInterface[]>{
-    const path = `/api/articles?sort[0]=date:desc&populate[cover]=1&populate[article_tags][fields][0]=id&populate[article_tags][fields][1]=name&fields[0]=title&fields[1]=author&fields[2]=date&fields[3]=abstract&fields[4]=slug`;
+    const path = `/api/articles?sort[0]=date:desc&populate[cover]=1&populate[article_tags][fields][0]=id&populate[article_tags][fields][1]=name&fields[0]=title&fields[1]=author&fields[2]=date&fields[3]=abstract&fields[4]=slug&fields[5]=externalArticle&fields[6]=externalArticleLink`;
     const res  = await publicFetch(path);
     return res.data;
 }
@@ -44,7 +46,7 @@ export async function getRelatedArticles(tags : {id : number}[]) : Promise<relat
     const idsFilter = "".concat(...tags.map((tag : {id : number}, i : number) : string => {
         return `&filters[article_tags][id][$in][${i}]=${tag.id}`;
     }));
-    const path = `/api/articles?sort[0]=date:desc${idsFilter}&populate[cover]=1&populate[article_tags][fields][0]=id&populate[article_tags][fields][1]=name&fields[0]=title&fields[1]=author&fields[2]=date&fields[3]=abstract&fields[4]=slug`;
+    const path = `/api/articles?sort[0]=date:desc${idsFilter}&populate[cover]=1&populate[article_tags][fields][0]=id&populate[article_tags][fields][1]=name&fields[0]=title&fields[1]=author&fields[2]=date&fields[3]=abstract&fields[4]=slug&fields[5]=externalArticle&fields[6]=externalArticleLink`;
     const res  = await publicFetch(path);
     return res.data;
 }
@@ -60,7 +62,7 @@ export default function RelatedArticles({articles, ...otherProps} : {articles: r
                     author = {article.attributes.author}
                     abstract = {article.attributes.abstract}
                     date = {new Date(article.attributes.date)}
-                    url = {"/news/"+article.attributes.slug}
+                    url = {article.attributes.externalArticle && article.attributes.externalArticleLink? article.attributes.externalArticleLink : "/news/"+article.attributes.slug}
                     initial = {i === 0}
                     img= {article.attributes.cover.data?.attributes}
                 />
